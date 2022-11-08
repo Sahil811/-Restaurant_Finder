05fd721aa5e91350f11fb83c218cb683774aca78
sahil <sahilsiddiqui.dev@gmail.com>
2022-11-08 20:01:43 +0530

    master: Leaflet component added

---
+++ src/components/Map/index.js
@@ -0,0 +1,31 @@
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import "leaflet-defaulticon-compatibility";
+import "./index.scss";
+
+const MapView = () => {
+  const position = [51.505, -0.09];
+  return (
+    <div className="mapView">
+      <MapContainer
+        style={{ height: "100%", width: "100%" }}
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default MapView;
